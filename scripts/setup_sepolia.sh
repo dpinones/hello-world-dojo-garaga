f896daf518616aa397ade6d572de7a26bc1fa51b
Damian Piñones <pinonesdamian@gmail.com>
2025-05-08 22:26:41 -0300

    Update contract addresses and class hashes in manifest_sepolia.json for wolf_game_zk3; modify setup_sepolia.sh to specify the sepolia network during build and migration commands.

--- scripts/setup_sepolia.sh
+++ scripts/setup_sepolia.sh
@@ -12,8 +12,9 @@ if [ -d "manifests" ]; then
     rm -rf "manifests"
 fi
 
-echo "sozo build && sozo inspect && sozo migrate"
+echo "sozo -P sepolia build && sozo -P sepolia inspect && sozo -P sepolia migrate"
 sozo -P sepolia build && sozo -P sepolia inspect && sozo -P sepolia migrate
 
 echo -e "\n✅ Setup finish!"
 
+# slot d create --tier basic hunter torii --rpc https://starknet-sepolia.public.blastapi.io/rpc/v0_7 --world 0x00eb8bdd3fa6d122b7b4586f43894074e69a93a7d14197e24c99147e9318e2b8 -v v1.2.1
